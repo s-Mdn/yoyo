25aadfc9f26da03043375ab97a965f27269c3716
zhangyunbin <1135534394@qq.com>
2022-03-31 16:57:38 +0800

    上线测试

--- main.js
+++ main.js
@@ -59,8 +59,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1175,
     height: 875,
-    // frame: false,
-    // titleBarStyle: 'hidden', // 隐藏边框
+    frame: false,
+    titleBarStyle: 'hidden', // 隐藏边框
     center: true,
     webPreferences: {
       nodeIntegration: true,
@@ -140,7 +140,7 @@ function launchVideoProcess(flag) {
 }
 
 function onReady() {
-  // launchVideoProcess()
+  launchVideoProcess()
   createWindow()
   checkUpdate()
 }
