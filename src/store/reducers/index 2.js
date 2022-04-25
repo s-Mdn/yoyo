f0bd293234ab7b7cc45a0506e709f86e84a1053d
yoyo <zhangyunbin@zhangyunbindeiMac.local>
2022-04-25 12:01:40 +0800

    文件报错

--- src/store/reducers/index 2.js
+++
@@ -1,9 +0,0 @@
-import PlayAutoReducer from './play-auto'
-import LoginReducer from './login'
-import ProfileReducer from './profile'
-
-export {
-  LoginReducer,
-  PlayAutoReducer,
-  ProfileReducer,
-}
